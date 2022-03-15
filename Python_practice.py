1612e857885b121fb32862e334176a0f3d04a598
amonjaras <100452376+amonjaras@users.noreply.github.com>
2022-03-15 13:02:07 -0400

    practice

---
+++ Python_practice.py
@@ -0,0 +1,43 @@
+#3.2.2 Execute Python files
+print("Hello World\n")
+
+#3.2.3 Data Types
+#integer
+print("3 is ", type(3))
+print("2019 is ", type(2019),'\n')
+ballots=1,341
+print("Ballots = ",ballots)
+print("Ballots is ", type(ballots),'\n')
+
+#floating-point
+print("73.81 is ", type(73.81),'\n')
+
+#strings
+print("Hello World is ", type("Hello World"),'\n')
+
+#Boolean
+print("True is ", type(True),'\n')
+
+#Creating Variables
+num_candidates=3
+winning_percentage=73.81
+candidate="Diane"
+won_election=True
+
+#Variables/Order of precedence
+print("5+2*3 =",5+2*3)
+print("8//5-3 =",8//5-3)
+print("8+22*2-4 =",8+22*2-4)
+print("16-3/2+7-1 =",16-3/2+7-1)
+print("3**3%5 =",3**3%5)
+print("5+9*3/2-4 =",5+9*3/2-4,'\n')
+
+#Grouping with parenthesis
+print("(5+2)*3 =",(5+2)*3)
+print("(8//5)-3 =",(8//5)-3)
+print("8+(22*(2-4)) =",8+(22*(2-4)))
+print("16-3/(2+7)-1 =",16-3/(2+7)-1)
+print("3**(3%5) =",3**(3%5),'\n')
+print("5+(9*3/2-4) =",5+(9*3/2-4), "vs 5+(9*3/(2-4)) =",5+(9*3/(2-4)),'\n')
+
+#3.2.5 Lists
