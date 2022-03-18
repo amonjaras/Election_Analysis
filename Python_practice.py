f5c5536f8579bd5ff03cbcd72e5c67ecae693ca9
amonjaras <100452376+amonjaras@users.noreply.github.com>
2022-03-17 22:26:39 -0400

    Decision statements

--- Python_practice.py
+++ Python_practice.py
@@ -137,3 +137,29 @@ print(counties_dict.get("Denver"),'\n')
 
 #list of dictionaries
 voting_data = []
+voting_data.append({"county":"Arapahoe", "registered_voters":422829})
+voting_data.append({"county":"Denver", "registered_voters":463353})
+voting_data.append({"county":"Jefferson", "registered_voters":432438})
+print(voting_data,'\n')
+print(len(voting_data),'\n')
+voting_data.insert(1,{"county":"El Paso", "registered_voters":461149})
+print(voting_data,'\n')
+voting_data.pop(0)
+print(voting_data,'\n')
+voting_data.remove({"county":"Denver", "registered_voters":463353})
+print(voting_data,'\n')
+voting_data.append({"county":"Denver", "registered_voters":463353})
+print(voting_data,'\n')
+voting_data.append({"county":"Arapahoe", "registered_voters":422829})
+print(voting_data,'\n')
+
+print("----------------------",'\n')
+
+#3.2.8 Decision statements
+# How many votes did you get?
+my_votes = int(input("How many votes did you get in the election? "))
+#  Total votes in the election
+total_votes = int(input("What is the total votes in the election? "))
+# Calculate the percentage of votes you received.
+percentage_votes = (my_votes / total_votes) * 100
+print("I received " + str(percentage_votes)+"% of the total votes.")
