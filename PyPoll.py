6f0eb533013c7bd8c48dcb6d83dedcec2e680f68
amonjaras <100452376+amonjaras@users.noreply.github.com>
2022-03-19 16:50:05 -0400

    Adding PyPoll.py file

--- PyPoll.py
+++ PyPoll.py
@@ -13,18 +13,44 @@ import csv
 # Assign a variable for the file to load and the path
 csvpath = os.path.join("Resources", "election_results.csv")
 
+# Assign a variable to save the file to a path
+txtcreate = os.path.join("analysis", "election_analysis.txt")
+
 # Open the election results and read the file
 with open(csvpath) as election_data:
 
+    #To do: read and analyze the date here
+
+    # Read the file object with the reader function
+    csvreader = csv.reader(election_data)
+
+    # Print the header row
+    headers = next(csvreader)
+    print(headers)
+
+    #Print each row in the csv file
+    #for row in csvreader:
+        #print(row)
+
     # Print the file object
-    print(election_data)
+    #print(election_data)
 
-# Create a filename variable to a direct or indirect path to the file
-txtcreate = os.path.join("analysis", "election_analysis.txt")
 
-# Using the open() function with the "w" mode we will write data to the file
-open(txtcreate,"w")
+"""
+# Using the open() function to open the file as a text file
+outfile = open(txtcreate,"w")
+
+# write some data to the file
+#outfile.write("Hello World")
+
+# write three countries to the file
+#outfile.write("Arapahoe \nDenver\nJefferson")
+
+# skill Drill
+outfile.write("Counties in the election \n-----------------------\nArapahoe\nDenver\nJefferson")
+
 
 # Close the file
-election_data.close()
+outfile.close()
 
+"""
