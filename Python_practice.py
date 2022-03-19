8f6868c08ccd2f6bbd4a0e8ab5af880de07a9d7b
amonjaras <100452376+amonjaras@users.noreply.github.com>
2022-03-19 10:55:03 -0400

    import and inspect data

--- Python_practice.py
+++ Python_practice.py
@@ -368,3 +368,42 @@ counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
 for county, voters in counties_dict.items():
     print(county + " county has " + str(voters) + " registered voters.")
 print('\n')
+
+#using f-string
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters")
+print('\n')
+
+#multiline F-strings
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes} number of votes. "
+    f"The total number of votes in the election was {total_votes}. "
+    f"You received {candidate_votes / total_votes * 100}% of the total votes.")
+print(message_to_candidate)
+print('\n')
+
+#format floating-point decimals
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes:,} number of votes. "
+    f"The total number of votes in the election was {total_votes:,}. "
+    f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
+print(message_to_candidate)
+print('\n')
+
+#skill drill 3.2.11
+
+print("this is a skill drill to use f-strings")
+voting_data = [
+    {"county":"Arapahoe", "registered_voters": 422829}, 
+    {"county":"Denver", "registered_voters": 463353},
+    {"county":"Jefferson", "registered_voters": 432438}]
+for county_dict in voting_data:
+    print(f"{county_dict['county']} county has {county_dict['registered_voters']} registered voters")
+print('\n')
+print("----------------------",'\n')
+
+#3.3.1 Import and inspect the data
